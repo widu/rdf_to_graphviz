7b8409564fb8e2cdeaea418f29e03afb35216b8f
widu <wdulek@gmail.com>
2016-05-21 23:24:56 +0200

    Extended options. Ver. 0.0.1. It's not compatible with v0.0.0.2!

--- README.txt
+++ README.txt
@@ -9,5 +9,12 @@ require 'rdf_to_graphviz'
 
 konwerter = RdfToGraphviz.new
 queryable = RDF::Graph.load("http://ruby-rdf.github.com/rdf/etc/doap.nt")
-str = konwerter.rdf_graph_to_dot({:rdf_graph => queryable})
+str = konwerter.rdf_graph_to_dot(queryable, :is_literal_object_uniq => false)
 GraphViz.parse_string(str).output( :png => "res_graph1.png" )
+
+
+____________________________________________________________________________
+
+Caution!
+
+It's not compatible with ver. 0.0.0.2!
--- lib/rdf_to_graphviz.rb
+++ lib/rdf_to_graphviz.rb
@@ -18,22 +18,34 @@ class RdfToGraphviz
 	end
 
 	##
-	# +rdf_graph_to_dot(options = {})+ generate simple dot graph from RDF::Graph
-	#  @option options [RDF::Graph] :rdf_graph
-	#  @option options [TrueClass]  :digraph - if true, graph has a direction - true is default
-	#  @option options [String]     :subject_shape - default = doublecircle
-	#  @option options [String]     :object_shape - default = circle
-	#  @option options [String]     :literal_object_shape - default = rectangle
+	# rdf_graph_to_dot(rdf_graph, options = {}) generate simple dot graph from RDF::Graph
+	#  @param  [RDF::Graph]             rdf_graph                
+	#  @param  [Hash{Symbol => Object}] options
+	#  @option options [TrueClass]  :digraph                  - default = true - if true, graph has a direction
+	#  @option options [String]     :subject_shape            - default = doublecircle
+	#  @option options [String]     :object_shape             - default = circle
+	#  @option options [String]     :literal_object_shape     - default = rectangle
+	#  @option options [String]     :subject_color            - default = red
+	#  @option options [String]     :object_color             - default = blue
+	#  @option options [String]     :predicate_color          - default = black
+	#  @option options [TrueClass]  :is_literal_object_uniq   - default = true
 	#  @return [String] - contain representation of RDF::Graph in Graphviz dot 
     #
-	def rdf_graph_to_dot(options = {})
+	def rdf_graph_to_dot(rdf_graph, options = {})
 		txt_dot = ""
 
-		options[:rdf_graph] ||= @graph
-		options[:digraph] ||= true
+		#options[:rdf_graph] ||= @graph
+		options[:digraph] = true if options[:digraph].nil?
 		options[:subject_shape] ||= "doublecircle"
 		options[:object_shape] ||= "circle"
 		options[:literal_object_shape] ||= "rectangle"
+		options[:subject_color] ||= "red"
+		options[:object_color] ||= "blue"
+		options[:predicate_color] ||= "black"
+		options[:is_literal_object_uniq] = true if options[:is_literal_object_uniq].nil?
+
+
+		n=0
 
 		if options[:digraph] == true
 			txt_dot << "\ndigraph digraph_1 {"
@@ -41,18 +53,28 @@ class RdfToGraphviz
 			txt_dot << "\ngraph graph_1 {"
 		end
 
-		options[:rdf_graph].each_statement do |statement|
+		rdf_graph.each_statement do |statement|
 			
 			s = term_name(statement[0])
 			o =  term_name(statement[2])
 
-			txt_dot << "\n\"#{s}\"[color=red, shape=#{options[:subject_shape]}];"
+			txt_dot << "\n\"#{s}\"[label=\"#{s}\", color=#{options[:subject_color]}, shape=#{options[:subject_shape]}];"
+
 			if statement[2].literal?
-				txt_dot << "\n\"#{o}\"[shape=#{options[:literal_object_shape]}];"
+				if options[:is_literal_object_uniq] == true
+				  txt_dot << "\n\"#{o}\"[label=\"#{o}\", shape=#{options[:literal_object_shape]}];"
+				else
+
+				  txt_dot << "\n#{"\"o"+n.to_s}\"[label=\"#{o}\", shape=#{options[:literal_object_shape]}];"
+				  
+				  o = "o"+n.to_s
+				  n += 1
+				end	
 			else
-				txt_dot << "\n\"#{o}\"[color=blue, shape=#{options[:object_shape]}];"
+				txt_dot << "\n\"#{o}\"[label=\"#{o}\", color=#{options[:object_color]}, shape=#{options[:object_shape]}];"
 			end
-			txt_dot << "\n\"#{s}\" -> \"#{o}\" [label=\"#{statement[1].pname}\", color=red];"
+
+			txt_dot << "\n\"#{s}\" -> \"#{o}\" [label=\"#{statement[1].pname}\", color=#{options[:predicate_color]}];"
 			
 		end
 		txt_dot << "}"
